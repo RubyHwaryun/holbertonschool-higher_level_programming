b59df77201af7ac7b0e736f730cf18535e57eb1c
RubyHwaryun <abiyev.rasul44@gmail.com>
2024-05-07 00:39:05 +0400

    v2 of task 2

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,32 +1,3 @@
 #!/usr/bin/python3
-print("abcdefghijklmnopqrstuvwxyz", sep='', end='')#!/usr/bin/python3
-print("abcdefghijklmnopqrstuvwxyz", sep='', end='')#!/usr/bin/python3
-print("abcdefghijklmnopqrstuvwxyz", sep='', end='')#!/usr/bin/python3
-print("abcdefghijklmnopqrstuvwxyz", sep='', end='')#!/usr/bin/python3
-print("abcdefghijklmnopqrstuvwxyz", sep='', end='')#!/usr/bin/python3
-print("abcdefghijklmnopqrstuvwxyz", sep='', end='')#!/usr/bin/python3
-print("abcdefghijklmnopqrstuvwxyz", sep='', end='')#!/usr/bin/python3
-print("abcdefghijklmnopqrstuvwxyz", sep='', end='')#!/usr/bin/python3
-print("abcdefghijklmnopqrstuvwxyz", sep='', end='')#!/usr/bin/python3
-print("abcdefghijklmnopqrstuvwxyz", sep='', end='')#!/usr/bin/python3
-print("abcdefghijklmnopqrstuvwxyz", sep='', end='')#!/usr/bin/python3
-print("abcdefghijklmnopqrstuvwxyz", sep='', end='')#!/usr/bin/python3
-print("abcdefghijklmnopqrstuvwxyz", sep='', end='')#!/usr/bin/python3
-print("abcdefghijklmnopqrstuvwxyz", sep='', end='')#!/usr/bin/python3
-print("abcdefghijklmnopqrstuvwxyz", sep='', end='')#!/usr/bin/python3
-print("abcdefghijklmnopqrstuvwxyz", sep='', end='')#!/usr/bin/python3
-print("abcdefghijklmnopqrstuvwxyz", sep='', end='')#!/usr/bin/python3
-print("abcdefghijklmnopqrstuvwxyz", sep='', end='')#!/usr/bin/python3
-print("abcdefghijklmnopqrstuvwxyz", sep='', end='')#!/usr/bin/python3
-print("abcdefghijklmnopqrstuvwxyz", sep='', end='')#!/usr/bin/python3
-print("abcdefghijklmnopqrstuvwxyz", sep='', end='')#!/usr/bin/python3
-print("abcdefghijklmnopqrstuvwxyz", sep='', end='')#!/usr/bin/python3
-print("abcdefghijklmnopqrstuvwxyz", sep='', end='')#!/usr/bin/python3
-print("abcdefghijklmnopqrstuvwxyz", sep='', end='')#!/usr/bin/python3
-print("abcdefghijklmnopqrstuvwxyz", sep='', end='')#!/usr/bin/python3
-print("abcdefghijklmnopqrstuvwxyz", sep='', end='')#!/usr/bin/python3
-print("abcdefghijklmnopqrstuvwxyz", sep='', end='')#!/usr/bin/python3
-print("abcdefghijklmnopqrstuvwxyz", sep='', end='')#!/usr/bin/python3
-print("abcdefghijklmnopqrstuvwxyz", sep='', end='')#!/usr/bin/python3
-print("abcdefghijklmnopqrstuvwxyz", sep='', end='')#!/usr/bin/python3
-print("abcdefghijklmnopqrstuvwxyz", sep='', end='')
+str='abcdefghijklmnopqrstuvwxyz'
+print(str, end='')
