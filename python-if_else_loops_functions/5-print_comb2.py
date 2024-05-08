b92fdc647ae2eac24c2f14bb50b88aca39604920
RubyHwaryun <abiyev.rasul44@gmail.com>
2024-05-08 14:32:19 +0400

    task 5 fixing

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(100):
   if i < 10:
-    print("0" + str(i), end=", ")
+    print("0{}".format(i), end=", ")
   else:
-    print(i, end=", ")
+    print("{}".format(i), end=", ")
