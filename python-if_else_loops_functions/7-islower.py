23b5cc4e7582924cc778263e8c192ff4d250b60d
RubyHwaryun <abiyev.rasul44@gmail.com>
2024-05-17 16:24:07 +0400

    task7

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,9 +1,9 @@
-#!/usr/bin/python3
 def islower(c):
-  if (c >= 'A' and c <= 'Z'):
-   print("{} is upper".format(c));
+    return ord('a') <= ord(c) <= ord('z')
 
-  elif (c >= 'a' and c <= 'z'):
-   print("{} is lower".format(c));
-  else:
-    False
+
+print(islower('a'))
+print(islower('A'))
+print(islower('z'))
+print(islower('Z'))  
+print(islower('1'))
