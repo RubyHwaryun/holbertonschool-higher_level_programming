05341755dc592396cdfd6214a69ad79adc95a7b0
RubyHwaryun <abiyev.rasul44@gmail.com>
2024-05-17 15:51:14 +0400

    error fix6

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -2,6 +2,6 @@
 
 for i in range(100):
     if i < 10:
-      print("0{}".format(i), end=", ")
+        print("0{}".format(i), end=", ")
     else:
-      print("{}".format(i), end=", " if i != 99 else "\n")
+        print("{}".format(i), end=", " if i != 99 else "\n")
