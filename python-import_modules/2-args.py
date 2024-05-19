46c6d1d2682834f1db8ff3dedf916f4c0b5e417b
RubyHwaryun <abiyev.rasul44@gmail.com>
2024-05-19 19:43:37 +0400

    PEP correction

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -9,5 +9,5 @@ if __name__ == "__main__":
         print(f"1: {sys.argv[1]}")
     elif len(sys.argv) >= 1:
         print(f"{len(sys.argv) - 1} arguments:")
-        for i in range(1,len(sys.argv)):
+        for i in range(1, len(sys.argv)):
             print(f"{i}: {sys.argv[i]}")
