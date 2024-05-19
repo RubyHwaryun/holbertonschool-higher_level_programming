7aa2c8cf254745a715ddcd4a6b5913a5f8fd8f0f
RubyHwaryun <abiyev.rasul44@gmail.com>
2024-05-19 19:42:51 +0400

    task2v8

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -8,6 +8,6 @@ if __name__ == "__main__":
         print("1 argument:")
         print(f"1: {sys.argv[1]}")
     elif len(sys.argv) >= 1:
-        print(f"{len(sys.argv) - 1} argumets:")
+        print(f"{len(sys.argv) - 1} arguments:")
         for i in range(1,len(sys.argv)):
             print(f"{i}: {sys.argv[i]}")
