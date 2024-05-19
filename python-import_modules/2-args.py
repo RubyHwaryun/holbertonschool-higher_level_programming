8d6d00cf5b7cf38789a5a5413ec6aa0252827c01
RubyHwaryun <abiyev.rasul44@gmail.com>
2024-05-19 19:36:05 +0400

    task2v1

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -4,6 +4,8 @@ if __name__ == "__main__":
     import sys
     if len(sys.argv) == 1:
         print("0 arguments.")
+    elif len(sys.argv) == 2:
+        print("1 argument:") 
     elif len(sys.argv) >= 1:
         print(f"{len(sys.argv) - 1} argumets:")
         for i in range(1,len(sys.argv)):
