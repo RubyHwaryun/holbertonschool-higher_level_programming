f8d629ea1a0bb9bcac6bef14425df07ed3aac71d
RubyHwaryun <abiyev.rasul44@gmail.com>
2024-05-26 19:33:28 +0400

    tskv5

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def no_c(my_string):
-    for i in range(len(my_string)):
+    for i in range(len(my_string) -1, -1, -1):
         if my_string[i] == "c":
             my_string = my_string[:i] + my_string[i+1:]
         if my_string[i] == "C":
