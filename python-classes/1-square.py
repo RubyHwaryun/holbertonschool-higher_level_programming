4ec11781b726f57236704edfe83fcd9b58da22a7
RubyHwaryun <abiyev.rasul44@gmail.com>
2024-05-26 23:49:45 +0400

    tskallv2

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,4 +1,15 @@
 #!/usr/bin/python3
 
+"""Define a class."""
+
+
 class Square:
-    self.__size = size
+    """Represent a square."""
+
+    def __init__(self, size):
+        """Initialize Square.
+
+        Args:
+            size (int): The size of the new square.
+        """
+        self.__size = sizei
