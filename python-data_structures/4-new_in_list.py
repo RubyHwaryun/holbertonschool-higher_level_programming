cb6332f350d4d2135dba7cac19a9491aabfdcce1
RubyHwaryun <abiyev.rasul44@gmail.com>
2024-05-26 19:09:56 +0400

    tskv1

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 def new_in_list(my_list, idx, element):
   new_in_list = my_list[:]
   if idx < 0:
--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 def no_c(my_string):
   for i in range(len(my_string)):
     if my_string[i] == "c":
