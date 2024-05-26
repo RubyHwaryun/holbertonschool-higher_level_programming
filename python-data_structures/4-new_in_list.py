cde13673d6d6056cd227580d0dd84af0cb79f454
RubyHwaryun <abiyev.rasul44@gmail.com>
2024-05-26 19:24:35 +0400

    tskv3

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 
 def new_in_list(my_list, idx, element):
-  new_in_list = my_list[:]
-  if idx < 0:
-    return new_in_list
-  if idx >= len(my_list):
-    return new_in_list
-  else:
-    new_in_list[idx] = element
-    return new_in_list
+    new_in_list = my_list[:]
+    if idx < 0:
+        return new_in_list
+    if idx >= len(my_list):
+        return new_in_list
+    else:
+        new_in_list[idx] = element
+        return new_in_list
