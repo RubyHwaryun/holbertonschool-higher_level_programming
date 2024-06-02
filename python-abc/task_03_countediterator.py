3eebc3ec203c6676387d272ba295e3c082ec7548
RubyHwaryun <abiyev.rasul44@gmail.com>
2024-06-02 22:33:06 +0400

    tskv2

--- python-abc/task_03_countediterator.py
+++ python-abc/task_03_countediterator.py
@@ -1,9 +1,12 @@
-#!/usr/bin/python3 
+#!/usr/bin/python3
 
 class CountedIterator:
     def __init__(self, iterable):
         self.iterator = iter(iterable)
         self.count = 0
+
+    def __iter__(self):
+        return self
 
     def __next__(self):
         try:
