e6b9f97cdd20e87aea36d36b3ca90218102ed33d
RubyHwaryun <abiyev.rasul44@gmail.com>
2024-07-25 18:36:15 +0400

    tsk1v2

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,5 +1,17 @@
 #!/usr/bin/python3
 
 def read_file(filename=""):
+"""
+    Reads the content of the specified file and returns it as a string.
+
+    Parameters:
+    filename (str): Optional. The name of the file to read. Defaults to reading 'my_file'.
+
+    Returns:
+    str: The content of the file as a string.
+
+    Raises:
+    IOError: If the file cannot be opened or if there is an input/output error while reading.
+"""
     with open('my_file', encoding="utf-8") as f:
 	read_data = f.read()
