469d53f961acd1d5b1b786332f542349e9619bbb
RubyHwaryun <abiyev.rasul44@gmail.com>
2024-08-08 16:42:17 +0400

    tsk1v4

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,26 +1,15 @@
 #!/usr/bin/python3
-
 """
-Module: file_operations
-
-This module provides functions for file operations, such as reading and writing files.
-
-Functions:
-- read_file(filename=""): Reads the content of the specified file and returns it as a string.
+This module defines a function to read and print the contents of a UTF-8 text file.
 """
 
 def read_file(filename=""):
     """
-    Reads the content of the specified file and returns it as a string.
+    Read a UTF-8 text file and print its contents to stdout.
 
     Parameters:
-    filename (str): Optional. The name of the file to read. Defaults to reading 'my_file'.
-
-    Returns:
-    str: The content of the file as a string.
-
-    Raises:
-    IOError: If the file cannot be opened or if there is an input/output error while reading.
+    filename (str): The path to the file to read. Defaults to an empty string.
     """
-    with open(filename, encoding="utf-8") as f:
-        read_data = f.read()
+    with open(filename, 'r', encoding='utf-8') as file:
+        print(file.read(), end='')
+
