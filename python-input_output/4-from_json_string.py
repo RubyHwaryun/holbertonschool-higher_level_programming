ebaacae1c7791cd7bd2add69527c181516c4b18f
RubyHwaryun <abiyev.rasul44@gmail.com>
2024-09-26 10:23:50 +0400

    tsk4

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Write a function that returns an object 
+"""
+
+import json
+
+
+def from_json_string(my_str):
+    """
+    Convert a JSON string to a Python object.
+
+    Parameters:
+    my_str (str): The JSON string to be converted to a Python object.
+
+    Returns:
+    any: A Python object representing the JSON string.
+    """
+    return json.loads(my_str)
